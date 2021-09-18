db6ba3e7f58cf72e71096626aded07df5bf464b6
doozy69 <37107974+doozy69@users.noreply.github.com>
2021-09-18 20:10:29 +0300

    Добавьте файлы проекта.

---
+++ WorldSync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorldSync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
